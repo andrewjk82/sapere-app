472f01d8d0548323aab3525455163633c5e8acf8
Andrew Kim <andrewkim@MacBookPro.tpg_nbn>
2026-04-28 18:29:39 +1000

    Update brand tagline to 'Dare to know'

--- src/pages/AuthLayout.jsx
+++ src/pages/AuthLayout.jsx
@@ -32,7 +32,7 @@ const AuthLayout = ({
             </div>
             <div>
               <p className="auth-brand__name">Sapere Aude Academia</p>
-              <p className="auth-brand__tag">Private tutor workspace</p>
+              <p className="auth-brand__tag">Dare to know</p>
             </div>
           </div>
 
