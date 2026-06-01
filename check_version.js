d81202b1f141e61de39b74f2bfedd50ee4b220dc
Andrew Kim <andrewkim@MacBookPro.nbn>
2026-06-01 17:48:00 +1000

    chore: untrack local helper/patch scripts, add to .gitignore

--- check_version.js
+++
@@ -1,7 +0,0 @@
-import { initializeApp } from 'firebase/app';
-import { getFirestore, doc, getDoc } from 'firebase/firestore';
-
-const firebaseConfig = {
-  apiKey: process.env.VITE_FIREBASE_API_KEY || "AIzaSy...", // I will just use the config from src/firebase/config.js
-};
-// Wait, a better way is to run a script that imports from the project directly
--- test_regex.js
+++
@@ -1,3 +0,0 @@
-const str = "$A \\cup B = \\{1, 2\\}$";
-const match = str.match(/^(.*?)(?:\s+|:\s*|,\s*)([\\\(|\$|\\\[].+?[\\\)|\\\]|\$])$/);
-console.log(match);
