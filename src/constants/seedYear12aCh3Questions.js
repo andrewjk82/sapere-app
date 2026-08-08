cc224919f74adf174d0a5c100f73aa7cad002659
Andrew Kim <andrewkim@MacBookPro.nbn>
2026-08-08 10:08:28 +1000

    fix: Update q21b and q21c JSXGraph scripts to use functiongraph instead of line so axes render correctly

--- src/constants/seedYear12aCh3Questions.js
+++ src/constants/seedYear12aCh3Questions.js
@@ -3502,7 +3502,7 @@ export const Y12A_CH3_QUESTIONS = [
           4,
           -4
         ],
-        "script": "board.suspendUpdate();\nboard.create('line', [[-3, -3], [3, 3]], {strokeColor: 'blue', strokeWidth: 2});\nboard.unsuspendUpdate();"
+        "script": "board.suspendUpdate();\nboard.create('functiongraph', [function(x){ return x; }], {strokeColor: 'blue', strokeWidth: 2});\nboard.unsuspendUpdate();"
       }
     }
   },
@@ -3530,7 +3530,7 @@ export const Y12A_CH3_QUESTIONS = [
           4,
           -4
         ],
-        "script": "board.suspendUpdate();\nboard.create('line', [[-3, 3], [3, -3]], {strokeColor: 'blue', strokeWidth: 2});\nboard.unsuspendUpdate();"
+        "script": "board.suspendUpdate();\nboard.create('functiongraph', [function(x){ return -x; }], {strokeColor: 'blue', strokeWidth: 2});\nboard.unsuspendUpdate();"
       }
     }
   },
